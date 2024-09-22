264abfca5b50d657fa50102f5cb63a84ccd5f637
lucianosoares748 <lucianosoaresdev748@gmail.com>
2024-09-22 12:18:32 +0100

    rising 19 and 20

---
+++ aula20.py
@@ -0,0 +1,7 @@
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor: ')
+
+if primeiro_valor >= segundo_valor:
+    print(f'O {primeiro_valor=} e maior ou igual ao {segundo_valor=}')
+else:
+    print(f'o {segundo_valor=} e maior que o {primeiro_valor=}')
