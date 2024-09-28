2a855ba1ad6d55d85b33b9b79d26d3f3016fb8b4
lucianosoares748 <lucianosoaresdev748@gmail.com>
2024-09-28 18:34:09 +0100

    commit_pratice

--- aula29.py
+++ aula29.py
@@ -4,8 +4,26 @@ try -> tentar executar o código
 except -> ocorreu algum erro ao tentar executar
 """
 
-numero_str = input('Vou duplicar o número que você Digitar: ')
+numero_str = input('Vou dobrar o número que você Digitar: ')
 
-numero_float = float(numero_str)
-print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
+try: # try -> tenta executar esse codigo abaixo:
+    numero_float = float(numero_str)
+    print('FLOAT:', numero_float)
+    print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
+except: # except -> Mas se no try ocorrer um erro "excessao" pula pro codigo abaixo:
+    print('Isso não é um número')
 
+
+# if numero_str.isdigit():
+
+#     numero_float = float(numero_str)
+#     print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
+# else:
+#     print('Isso nao e um numero')
+
+
+
+#  -----------------ANOTACOES
+#SEMPRE que o usuario enviar algo seja atraves do input, essa dado 
+#PRECISA ser tratado
+#Toda variavel(objeto) tem atributos para serem usado basta digitar . 
---
+++ aula30.py
@@ -0,0 +1,11 @@
+"""
+CONSTANTE = "Variáveis" que não vão mudar
+Muitas condições no mesmo if (ruim)
+    <- Contagem de complexidade (ruim)
+"""
+velocidade = 61 #velocidade atual do carro
+local_carro = 90 #Local em que o carro esta na estrada KM
+
+RADAR_1 = 60 # velocidade maxima do radar 1
+LOCAL_1 = 100 # Local onde o radar 1 esta
+RADDAR_RANGE = 1 # A A distância onde o radar pega
