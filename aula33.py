647c98d32378644f07a6723f93f82abdc04b43d4
Luciano Soares <lucianosoaresdev748@gmail.com>
2024-10-21 22:42:56 -0400

    commit_aula58

---
+++ aula33.py
@@ -0,0 +1,14 @@
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vimos: str, int, float, bool
+"""
+string = 'luciano soares'
+outra_variavel = f'{string[:3]}ABC{string[4:]}'
+print(string)
+print(outra_variavel)
+print(string.capitalize())
+
+'''
+Dentro de objetos podemos executar algumas ações que estão na 
+documentação do python, como a função capitalize(). 
+'''
