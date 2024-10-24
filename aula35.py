a97443ab0952f698226070786473a6b22fcacc01
Luciano Soares <lucianosoaresdev748@gmail.com>
2024-10-23 21:29:45 -0400

    commit_aula60

---
+++ aula35.py
@@ -0,0 +1,14 @@
+"""
+Criei um programinha para contar de 0 até 100 e quando o número 
+do contador for maior que sem, ele se tornará falso e irá interromper 
+a execução do código que está no while e parar o programa
+"""
+
+
+contador = 0
+
+while contador < 100:
+    contador = contador + 1
+    print(contador)
+
+print('Fim')
